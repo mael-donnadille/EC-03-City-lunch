6dbe16d00cdfc746b32af5e920b0a52e0010d186
mael-donnadille <mael.donnadille@gmail.com>
2026-05-05 10:45:35 +0200

    Affichage de la liste des produits

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ProductController extends AbstractController
+{
+    #[Route('/products', name: 'app_product_index')]
+    public function index(ProductRepository $productRepository): Response
+    {
+        $products = $productRepository->findBy(
+            ['available' => true],
+            ['type' => 'ASC', 'name' => 'ASC']
+        );
+
+        return $this->render('product/index.html.twig', [
+            'products' => $products,
+        ]);
+    }
+}
